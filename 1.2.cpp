69c24b4f6d4d4ec3fb82b3420d5a9c5ef66eea7b
JackPhone <149944996+zhangyuesb@users.noreply.github.com>
2023-11-24 09:35:11 +0800

    Add files via upload

--- 1.2.cpp
+++ 1.2.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-
+#define pi 3.14//使用标识符常量定义圆周率
 
 int main()
 {
@@ -9,7 +9,6 @@ int main()
 	cin >> r;
 	cout << "请输入圆锥的高(m)";
 	cin >> h;
-	float pi = 3.14;//使用标识符常量定义圆周率
 	V = (pi * r * r * h) / 3;//体积公式计算圆锥体积
 	cout << "这个圆锥的体积为" << V << "m^3" << endl;
 	return 0;
